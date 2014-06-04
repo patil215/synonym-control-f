6a8ff96aab8daab0babc83d5a12355f922682d52
patil215 <neilpatil215@gmail.com>
2014-06-03 19:38:14 -0500

    highlighting done on submit button click

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log("Highlighting synonyms on page: " + tab.url);
+function hello() {
+	console.log("Highlighting synonyms on page: ");
 	chrome.tabs.executeScript(null, {file:"jquery.js"});
 	chrome.tabs.executeScript(null, {file:"highlight.js"});
 	chrome.tabs.executeScript(null, {file: "synonymhighlight.js"});
-});
+}
+document.getElementById('submit').addEventListener('click', hello);

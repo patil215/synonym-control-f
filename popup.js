57dffc9e6b83dcd9bebf9d50eb8d4358bc5969d0
Neil Patil <npatil@indeed.com>
2014-06-29 21:15:52 -0700

    Go previous if shift held down with enter

--- popup.js
+++ popup.js
@@ -1,82 +1,77 @@
 var lastQueryInput;
-
-
 window.onload = function() {
-  document.getElementById("query").focus();
+    document.getElementById("query").focus();
 };
 var tabId = 0;
-
 $(document).ready(function() {
-	chrome.tabs.getSelected(null, function(tab) {
-		tabId = tab.id;
-		chrome.tabs.executeScript(tabId, {
-			file: "jquery.js"
-		});
-		chrome.tabs.executeScript(tabId, {
-			file: "highlight.js"
-		});
-		chrome.tabs.executeScript(tabId, {
-			file: "jquery.scrollTo.min.js"
-		});
-		chrome.tabs.executeScript(tabId, {
-			file: 'synonymhighlight.js'
-		}, function() {
-			chrome.tabs.sendMessage(tabId, ["initialize", ""]);
-		});
-	});
+    chrome.tabs.getSelected(null, function(tab) {
+        tabId = tab.id;
+        chrome.tabs.executeScript(tabId, {
+            file: "jquery.js"
+        });
+        chrome.tabs.executeScript(tabId, {
+            file: "highlight.js"
+        });
+        chrome.tabs.executeScript(tabId, {
+            file: "jquery.scrollTo.min.js"
+        });
+        chrome.tabs.executeScript(tabId, {
+            file: 'synonymhighlight.js'
+        }, function() {
+            chrome.tabs.sendMessage(tabId, ["initialize", ""]);
+        });
+    });
 });
-
 chrome.runtime.onMessage.addListener(function(message, sender) {
-	switch(message.command) {
-		case "IndexUI":
-			if(message.total == 0) {
-				$("#indexText").text("0 of 0");
-			} else {
-				$("#indexText").text((message.currentIndex + 1) + " of " + message.total);
-			}
-			break;
-		case "TextBox":
-			$("#query").val(message.queryText);
-			$("#query").focus();
-			break;
-	}
+    switch (message.command) {
+        case "IndexUI":
+            if (message.total == 0) {
+                $("#indexText").text("0 of 0");
+            } else {
+                $("#indexText").text((message.currentIndex + 1) + " of " + message.total);
+            }
+            break;
+        case "TextBox":
+            $("#query").val(message.queryText);
+            $("#query").focus();
+            break;
+    }
 });
-
-
-$("#query").keyup(function (e) {
+$("#query").keyup(function(e) {
+    // If shift key held down, go previous
     if (e.keyCode == 13) {
-    	// If it's the same query, go to the next result, otherwise, do a new search
-    	if($("#query").val() == lastQueryInput) {
-    		chrome.tabs.sendMessage(tabId, ["next", ""]);
-    	} else {
-        	search();
-    	}
+        if (e.shiftKey) {
+            chrome.tabs.sendMessage(tabId, ["previous", ""]);
+        } else {
+            // If it's the same query, go to the next result, otherwise, do a new search
+            if ($("#query").val() == lastQueryInput) {
+                chrome.tabs.sendMessage(tabId, ["next", ""]);
+            } else {
+                search();
+            }
+        }
     }
 });
 
 function search() {
-	console.log("Highlighting synonyms on page: ");
-	query = $("#query").val();
-	lastQueryInput = query;
-	chrome.tabs.sendMessage(tabId, ["dehighlight", ""]);
-	chrome.tabs.sendMessage(tabId, ["query", query]);
+    console.log("Highlighting synonyms on page: ");
+    query = $("#query").val();
+    lastQueryInput = query;
+    chrome.tabs.sendMessage(tabId, ["dehighlight", ""]);
+    chrome.tabs.sendMessage(tabId, ["query", query]);
 }
-
 $("#submit").click(function() {
-	search();
+    search();
 });
-
 $("#dehighlight").click(function() {
-	$("#indexText").text("0 of 0");
-	$("#query").val("");
-	$("#query").focus();
-	chrome.tabs.sendMessage(tabId, ["dehighlight", ""]);
+    $("#indexText").text("0 of 0");
+    $("#query").val("");
+    $("#query").focus();
+    chrome.tabs.sendMessage(tabId, ["dehighlight", ""]);
 });
-
 $("#previous").click(function() {
-	chrome.tabs.sendMessage(tabId, ["previous", ""]);
+    chrome.tabs.sendMessage(tabId, ["previous", ""]);
 });
-
 $("#next").click(function() {
-	chrome.tabs.sendMessage(tabId, ["next", ""]);
+    chrome.tabs.sendMessage(tabId, ["next", ""]);
 });
